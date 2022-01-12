9f86370a914fe976c1b5c15e3eba40e43103b061
Grant Spraker <grant.spraker@gmail.com>
2022-01-12 11:58:40 -0700

    got to slide 5

--- Mapper/ViewController.swift
+++ Mapper/ViewController.swift
@@ -1,14 +1,16 @@
-//
-//  ViewController.swift
-//  Mapper
-//
-//  Created by Grant Spraker on 1/11/22.
-//
+/*
+  ViewController.swift
+  Mapper
+
+  Created by Grant Spraker on 1/11/22.
+*/
 
 import MapKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, CLLocationManagerDelegate
 {
+    var currentLocation: CLLocation!
+    
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
@@ -17,11 +19,21 @@ class ViewController: UIViewController
     {
         super.viewDidLoad()
         
-        let locationManager.requestWhenInUseAuthorization()
-        mapView.showUserLocation = true
-
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+     currentLocation = locations[0]
+    }
+    
+    @IBAction func ZoomButton(_ sender: Any)
+    {
+        
+    }
 }
 
