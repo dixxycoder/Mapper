aca1ac06ba426170327b5ef654be4e4e1563d2dc
Grant Spraker <grant.spraker@gmail.com>
2022-01-13 11:11:12 -0700

    got all of the code for the MVP done

--- Mapper/ViewController.swift
+++ Mapper/ViewController.swift
@@ -9,11 +9,14 @@ import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate
 {
-    var currentLocation: CLLocation!
     
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
+    
+    var currentLocation: CLLocation!
+    
+    var parks: [MKMapItem] = []
     
     override func viewDidLoad()
     {
@@ -31,9 +34,41 @@ class ViewController: UIViewController, CLLocationManagerDelegate
      currentLocation = locations[0]
     }
     
-    @IBAction func ZoomButton(_ sender: Any)
+    @IBAction func ZoomButton(_ sender: UIBarButtonItem)
     {
+        let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: coordinateSpan)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    @IBAction func whenSearch(_ sender: Any)
+    {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Parks"
         
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        
+        let search = MKLocalSearch(request: request)
+        search.start
+        {
+            (response, error) in
+        guard let response = response else
+        
+        { return }
+            
+        for mapItem in response.mapItems
+            {
+            
+        self.parks.append(mapItem)
+        let annotation = MKPointAnnotation()
+            
+        annotation.coordinate = mapItem.placemark.coordinate
+        annotation.title = mapItem.name
+            
+        self.mapView.addAnnotation(annotation)
+            }
+        }
     }
 }
-
